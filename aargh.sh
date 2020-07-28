4e4682e9623abd1d238a09146513697c65476d33
Francesco Minnocci <ad17fmin@uwcad.it>
2020-07-28 15:25:14 +0200

    Fix libxft-bgra

--- aargh.sh
+++ aargh.sh
@@ -219,7 +219,7 @@ ntpdate 0.us.pool.ntp.org >/dev/null 2>&1
 installationloop
 
 dialog --title "AARGH Installation" --infobox "Finally, installing \`libxft-bgra\` to enable color emoji in suckless software without crashes." 5 70
-yes | sudo -u "$name" $aurhelper -S libxft-bgra >/dev/null 2>&1
+sudo -u "$name" $aurhelper -S --noconfirm libxft-bgra >/dev/null 2>&1
 
 # Install the dotfiles in the user's home directory
 putgitrepo
