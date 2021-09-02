4b593c595aecdd06e986ae54518bd81bbbfdb836
Francesco Minnocci <ad17fmin@uwcad.it>
2021-09-03 01:10:26 +0200

    Also, don't do `--depth 1`

--- aargh.sh
+++ aargh.sh
@@ -101,7 +101,7 @@ gitmakeinstall() {
 	progname="$(basename "$1" .git)"
 	dir="$repodir/$progname"
 	dialog --title "AARGH Installation" --infobox "Installing \`$progname\` ($n of $total) via \`git\` and \`make\`. $(basename "$1") $2" 5 70
-	sudo -u "$name" git clone --depth 1 "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$name" git pull origin master;}
+	sudo -u "$name" git clone "$1" "$dir" >/dev/null 2>&1 || { cd "$dir" || return ; sudo -u "$name" git pull origin master;}
 	cd "$dir" || exit
 	make >/dev/null 2>&1
 	make install >/dev/null 2>&1
